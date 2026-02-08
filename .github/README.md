b6474097db6d344228bfeadd5385c4a9fc57cea7
mudkipdev <mudkip@mudkip.dev>
2026-02-08 01:49:56 -0700

    initial commit

---
+++ .github/README.md
@@ -0,0 +1,2 @@
+# server
+Vanilla survival multiplayer in Minestom
---
+++ build.gradle.kts
@@ -0,0 +1,27 @@
+plugins {
+    java
+    application
+    id("com.gradleup.shadow") version "9.3.1"
+}
+
+group = "rocks.minestom"
+version = "0.1.0"
+application.mainClass = "rocks.minestom.Server"
+
+java {
+    toolchain.languageVersion = JavaLanguageVersion.of(25)
+    withSourcesJar()
+    withJavadocJar()
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("net.minestom:minestom:2026.01.08-1.21.11")
+    implementation("io.github.togar2:MinestomPvP:2025.12.29-1.21.11")
+    implementation("org.tinylog:tinylog-api:2.8.0-M1")
+    implementation("org.tinylog:tinylog-impl:2.8.0-M1")
+    implementation("org.tinylog:slf4j-tinylog:2.8.0-M1")
+}
